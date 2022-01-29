dbf88ce75a3912ee89b0bf41437b16a8fceb97c0
ZAX-211 <98634687+ZAX-211@users.noreply.github.com>
2022-01-29 13:55:40 +0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,1 +1,7 @@
-print("Эта мразь наконец-то запустилась")
+var_1 = 1567
+reverse = 0
+while var_1 > 0:
+    rest = var_1 % 10
+    reverse = reverse * 10 + rest
+    var_1 = var_1 // 10
+print("Число в обратном порядке:", reverse)
